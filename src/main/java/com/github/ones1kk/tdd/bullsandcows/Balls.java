d0328b0cfed7d33faa4f19f0b71adadcf80d477e
ones1kk <ones1k95@gmail.com>
2022-07-09 18:25:44 +0900

    ✨ Add : Balls

---
+++ src/main/java/com/github/ones1kk/tdd/bullsandcows/Balls.java
@@ -0,0 +1,12 @@
+package com.github.ones1kk.tdd.bullsandcows;
+
+import java.util.List;
+
+public class Balls {
+
+    private List<Ball> ballList;
+
+    public Balls(List<Ball> ballList) {
+        this.ballList = ballList;
+    }
+}
