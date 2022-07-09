9aa0d82b4bacab7e03677057ff79137a2c036fba
ones1kk <ones1k95@gmail.com>
2022-07-09 17:57:49 +0900

    ✅ Add : Validate InvalidValueTest

--- src/test/java/com/github/ones1kk/tdd/BullsAndCowsTest.java
+++ src/test/java/com/github/ones1kk/tdd/BullsAndCowsTest.java
@@ -1,10 +1,12 @@
 package com.github.ones1kk.tdd;
 
 import com.github.ones1kk.tdd.bullsandcows.Ball;
+import com.github.ones1kk.tdd.bullsandcows.exception.InvalidValueException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BullsAndCowsTest {
 
@@ -12,12 +14,27 @@ public class BullsAndCowsTest {
     @DisplayName("create Ball class")
     public void test1() throws Exception {
         // given
-        int num = 1;
+        int value = 1;
 
         // when
-        Ball ball = new Ball(num);
+        Ball ball = new Ball(value);
 
         //then
+        assertThat(ball).isInstanceOf(Ball.class);
+    }
+
+    @Test
+    @DisplayName("Int value of Ball class is between 1~9 ?")
+    public void test2() throws Exception {
+        // given
+        int invalidValue = 10;
+        int validValue = 5;
+
+        // when
+        Ball ball = new Ball(validValue);
+
+        //then
+        assertThrows(InvalidValueException.class, () -> new Ball(invalidValue));
         assertThat(ball).isInstanceOf(Ball.class);
     }
 }
