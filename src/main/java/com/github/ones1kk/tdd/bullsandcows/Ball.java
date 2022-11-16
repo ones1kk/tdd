f7a6ef23f47c2f57760e02154a8017c896e6ad77
ones1kk <ones1k95@gmail.com>
2022-11-16 16:57:56 +0900

    Add : Ball

--- src/main/java/com/github/ones1kk/tdd/bullsandcows/Ball.java
+++ src/main/java/com/github/ones1kk/tdd/bullsandcows/Ball.java
@@ -1,21 +1,26 @@
 package com.github.ones1kk.tdd.bullsandcows;
 
-import com.github.ones1kk.tdd.bullsandcows.exception.InvalidValueException;
-
-import static com.github.ones1kk.tdd.bullsandcows.Validator.isValidRange;
+import io.github.ones1kk.assertion.core.Asserts;
 
 public class Ball {
 
     private final int value;
 
-    public Ball(int value) {
-        boolean valid = isValidRange(value);
-        if (!valid) throw new InvalidValueException("range of value should be between 1 and 9");
+    private final int position;
+
+    public Ball(int value, int position) {
         this.value = value;
-    }
+        this.position = position;
+        Asserts.that(value)
+            .as("The value of ball should be greater than or equal to 1 and less than or equal to 9.")
+            .isNotNull()
+            .isGreaterThanOrEqualTo(1)
+            .isLessThanOrEqualTo(9);
 
-    public int getValue() {
-        return this.value;
+        Asserts.that(position)
+            .as("The position of ball should be greater than or equal to 1 and less than or equal to 3.")
+            .isNotNull()
+            .isGreaterThanOrEqualTo(1)
+            .isLessThanOrEqualTo(3);
     }
-
 }
