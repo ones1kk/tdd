5265f9fe8e5bfdd156455fae877800bfbdfc7c06
ones1kk <ones1k95@gmail.com>
2022-11-16 16:56:54 +0900

    Remove : All of files

--- src/main/java/com/github/ones1kk/tdd/bullsandcows/MainClass.java
+++
@@ -1,9 +0,0 @@
-package com.github.ones1kk.tdd.bullsandcows;
-
-public class MainClass {
-
-    public static void main(String[] args) {
-        Player player = new Player();
-        player.create().play();
-    }
-}
