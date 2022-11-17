cb60dc7985a4373100066752222e809373fcba83
ones1kk <ones1k95@gmail.com>
2022-11-17 14:52:29 +0900

    Update : ResultPrinterTest

--- src/test/java/com/github/ones1kk/tdd/bullsandcows/ResultPrinterTest.java
+++ src/test/java/com/github/ones1kk/tdd/bullsandcows/ResultPrinterTest.java
@@ -1,21 +1,69 @@
 package com.github.ones1kk.tdd.bullsandcows;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 class ResultPrinterTest {
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
-    @DisplayName("print test")
-    void printTest(int count) {
-        String message = ResultPrinter.print(count, count);
+    @Nested
+    @DisplayName("create test")
+    class CreateTest {
 
-        String result = String.format("%s Ball, %s Strike", count, count);
-        assertThat(message).isEqualTo(result);
+        @Test
+        @DisplayName("create success test")
+        void create_success() {
+            assertThatNoException().isThrownBy(() -> new ResultPrinter(0, 3));
+            assertThatNoException().isThrownBy(() -> new ResultPrinter(1, 2));
+            assertThatNoException().isThrownBy(() -> new ResultPrinter(2, 1));
+            assertThatNoException().isThrownBy(() -> new ResultPrinter(3, 0));
+        }
+
+        @Test
+        @DisplayName("create fail test, because of exceeding sum limit")
+        void create_fail_01() {
+            assertThatThrownBy(() -> new ResultPrinter(1, 3)).isInstanceOf(Exception.class);
+            assertThatThrownBy(() -> new ResultPrinter(2, 2)).isInstanceOf(Exception.class);
+            assertThatThrownBy(() -> new ResultPrinter(3, 1)).isInstanceOf(Exception.class);
+            assertThatThrownBy(() -> new ResultPrinter(4, 0)).isInstanceOf(Exception.class);
+        }
     }
 
+    @Nested
+    class PrintTest {
+
+        @Test
+        @DisplayName("print test")
+        void print_01() {
+            ResultPrinter resultPrinter = new ResultPrinter(1, 2);
+            String message = resultPrinter.getResult();
+
+            String expected = String.format("%s Ball, %s Strike", 1, 2);
+            assertThat(message).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("print test, when game is over")
+        void print_02() {
+            ResultPrinter resultPrinter = new ResultPrinter(0, 3);
+            String message = resultPrinter.getResult();
+
+            String expected = "You got all 3 numbers right!\n Game Over";
+            assertThat(message).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("print test, when it's nothing")
+        void print_03() {
+            ResultPrinter resultPrinter = new ResultPrinter(0, 0);
+            String message = resultPrinter.getResult();
+
+            String expected = "Nothing";
+            assertThat(message).isEqualTo(expected);
+        }
+    }
 }
