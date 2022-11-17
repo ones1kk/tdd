8957031dcd6afd99c4551fda6c7c935db3b0c0a7
ones1kk <ones1k95@gmail.com>
2022-11-17 09:20:27 +0900

    Update : Ball > isBall() & isStrike()

--- src/main/java/com/github/ones1kk/tdd/bullsandcows/Ball.java
+++ src/main/java/com/github/ones1kk/tdd/bullsandcows/Ball.java
@@ -9,8 +9,6 @@ public class Ball {
     private final int position;
 
     public Ball(int value, int position) {
-        this.value = value;
-        this.position = position;
         Asserts.that(value)
             .as("The value of ball should be greater than or equal to 1 and less than or equal to 9.")
             .isNotNull()
@@ -22,5 +20,24 @@ public class Ball {
             .isNotNull()
             .isGreaterThanOrEqualTo(1)
             .isLessThanOrEqualTo(3);
+        this.value = value;
+        this.position = position;
     }
+
+    public boolean isStrike(Ball ball) {
+        return isEqualValue(ball) && isEqualPosition(ball);
+    }
+
+    public boolean isBall(Ball ball) {
+        return isEqualValue(ball) && !isEqualPosition(ball);
+    }
+
+    private boolean isEqualValue(Ball ball) {
+        return this.value == ball.value;
+    }
+
+    private boolean isEqualPosition(Ball ball) {
+        return this.position == ball.position;
+    }
+
 }
