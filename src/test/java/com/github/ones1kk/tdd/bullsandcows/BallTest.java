64fc9f71e18d00d4cbeca55296e3c987746bd216
ones1kk <ones1k95@gmail.com>
2022-11-17 09:20:07 +0900

    Update : BallTest > IsBall() & isStrike()

--- src/test/java/com/github/ones1kk/tdd/bullsandcows/BallTest.java
+++ src/test/java/com/github/ones1kk/tdd/bullsandcows/BallTest.java
@@ -43,13 +43,81 @@ class BallTest {
     }
 
     @Nested
-    class ValueValidation {
-
+    @DisplayName("ball isStrike test")
+    class isStrikeTest {
 
         @Test
-        void isBallTest() {
-            Ball ball = new Ball(1, 1);
+        @DisplayName("ball.isStrike success test")
+        void isStrikeTest_success() {
+            Ball actual = new Ball(1, 1);
+            Ball expected = new Ball(1, 1);
 
+            boolean isStrike = actual.isStrike(expected);
+
+            assertThat(isStrike).isTrue();
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {2, 3})
+        @DisplayName("ball.isStrike fail test, same value & different position")
+        void isStrikeTest_fail_01(int position) {
+            Ball actual = new Ball(1, 1);
+            Ball expected = new Ball(1, position);
+
+            boolean isStrike = actual.isStrike(expected);
+
+            assertThat(isStrike).isFalse();
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {2, 3, 4, 5, 6, 7, 8, 9})
+        @DisplayName("ball.isStrike fail test, different value & same position")
+        void isStrikeTest_fail_02(int value) {
+            Ball actual = new Ball(1, 1);
+            Ball expected = new Ball(value, 1);
+
+            boolean isStrike = actual.isStrike(expected);
+
+            assertThat(isStrike).isFalse();
+        }
+    }
+
+    @Nested
+    @DisplayName("ball isBall test")
+    class isBallTest {
+
+        @Test
+        @DisplayName("ball.isBall success test, same value & different position")
+        void isBallTest_success() {
+            Ball actual = new Ball(1, 1);
+            Ball expected = new Ball(1, 3);
+
+            boolean isBall = actual.isBall(expected);
+
+            assertThat(isBall).isTrue();
+        }
+
+        @Test
+        @DisplayName("ball.isBall fail test, same value & position")
+        void isBallTest_fail_01() {
+            Ball actual = new Ball(1, 1);
+            Ball expected = new Ball(1, 1);
+
+            boolean isBall = actual.isBall(expected);
+
+            assertThat(isBall).isFalse();
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {2, 3, 4, 5, 6, 7, 8, 9})
+        @DisplayName("ball.isBall fail test, different value & same position")
+        void isBallTest_fail_02(int value) {
+            Ball actual = new Ball(1, 1);
+            Ball expected = new Ball(value, 1);
+
+            boolean isBall = actual.isBall(expected);
+
+            assertThat(isBall).isFalse();
         }
     }
 }
