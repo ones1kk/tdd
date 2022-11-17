95231fa59dd176c9ff5fd4dc03954cc0206eabb0
ones1kk <ones1k95@gmail.com>
2022-11-17 14:52:20 +0900

    Update : ResultPrinter

--- src/main/java/com/github/ones1kk/tdd/bullsandcows/ResultPrinter.java
+++ src/main/java/com/github/ones1kk/tdd/bullsandcows/ResultPrinter.java
@@ -1,9 +1,14 @@
 package com.github.ones1kk.tdd.bullsandcows;
 
+import io.github.ones1kk.assertion.core.Asserts;
+
 public class ResultPrinter {
 
-    private ResultPrinter() {
-    }
+    private final int ballCount;
+
+    private final int strikeCount;
+
+    private String result;
 
     private static final String BOTH = "%s Ball, %s Strike";
 
@@ -11,32 +16,60 @@ public class ResultPrinter {
 
     private static final String ONLY_STRIKE = "%s Strike";
 
-    public static String print(int ballCount, int strikeCount) {
-        String result = "";
-        if (isOnlyBall(ballCount, strikeCount)) {
-            result = String.format(ONLY_STRIKE, strikeCount);
+    private static final String NOTHING = "Nothing";
+
+    private static final String END = "You got all 3 numbers right!\n Game Over";
+
+    public ResultPrinter(int ballCount, int strikeCount) {
+        Asserts.that(ballCount + strikeCount)
+            .as("The sum of ballCount and strikeCount should be less than or equal to 3. but it's not(ballCount{}, strikeCount{})",
+                ballCount, strikeCount)
+            .isLessThanOrEqualTo(3);
+        this.ballCount = ballCount;
+        this.strikeCount = strikeCount;
+    }
+
+    public String getResult() {
+        if (isEnd()) {
+            result = END;
         }
 
-        if (isBoth(ballCount, strikeCount)) {
+        if(isNothing()) {
+            result = NOTHING;
+        }
+
+        if (isOnlyBall()) {
+            result = String.format(ONLY_BALL, strikeCount);
+        }
+
+        if (isBoth()) {
             result = String.format(BOTH, ballCount, strikeCount);
         }
 
-        if (isOnlyStrike(ballCount, strikeCount)) {
-            result = String.format(ONLY_BALL, ballCount);
+        if (isOnlyStrike()) {
+            result = String.format(ONLY_STRIKE, ballCount);
         }
 
         return result;
     }
 
-    private static boolean isBoth(int ballCount, int strikeCount) {
-        return ballCount != 0 && strikeCount != 0;
+    private boolean isBoth() {
+        return ballCount > 0 && strikeCount > 0;
     }
 
-    private static boolean isOnlyStrike(int ballCount, int strikeCount) {
-        return ballCount != 0 && strikeCount == 0;
+    private boolean isOnlyStrike() {
+        return ballCount == 0 && strikeCount > 0 && strikeCount < 3;
     }
 
-    private static boolean isOnlyBall(int ballCount, int strikeCount) {
-        return ballCount == 0 && strikeCount != 0;
+    private boolean isOnlyBall() {
+        return ballCount > 0 && strikeCount == 0;
+    }
+
+    private boolean isEnd() {
+        return ballCount + strikeCount == 3;
+    }
+
+    private boolean isNothing() {
+        return ballCount == 0 && strikeCount == 0;
     }
 }
